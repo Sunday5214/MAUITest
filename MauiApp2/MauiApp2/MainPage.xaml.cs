180acdb93b6c0c0a471f8cdf81d315656181d856
tokim <kimtaeo5214@gmail.com>
2023-10-16 18:15:56 +0900

    add. project

---
+++ MauiApp2/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnSendCode.PropertyChanged += BtnSendCode_PropertyChanged;
+            container.PropertyChanged += Input_PropertyChanged;
+        }
+
+        private void Input_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == "Width")
+            {
+                Debug.WriteLine(container.Width);
+            }
+        }
+
+        private void BtnSendCode_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Width")
+            {
+                Debug.WriteLine(btnSendCode.Width);
+            }
+        }
+    }
+}
